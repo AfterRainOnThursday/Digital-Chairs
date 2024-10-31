99479bd1ac30a33020bea041111f08693f834f4b
Sofia Akselevich <145220481+AfterRainOnThursday@users.noreply.github.com>
2024-10-31 20:09:55 +0300

    Update task_2_2_lab.py

--- task_2_2_lab.py
+++ task_2_2_lab.py
@@ -1,3 +1,5 @@
+import math
+
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
@@ -12,7 +14,6 @@ for month in range(months):
     money_capital += deficit
     spend *= (1 + increase)
 
-money_capital = round(money_capital)
+money_capital = math.ceil(money_capital)
 
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
-
